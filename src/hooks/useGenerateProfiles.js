cfa8559043a02f51a086d5659e7002c33045205f
Karan-b-5 <127388247+Karan-b-5@users.noreply.github.com>
2025-01-20 10:12:57 +0530

    Updated

--- src/hooks/useGenerateProfiles.js
+++ src/hooks/useGenerateProfiles.js
@@ -2,11 +2,11 @@ import { faker } from '@faker-js/faker'
 
 const useGenerateProfiles = (num) => {
   const profiles = [];
-  const newYork = { lat: 40.7128, lng: -74.0060 }; // Central coordinates of New York City
-  const centralLat = 40.7128; // Latitude for New York City 
-  const centralLng = -74.0060; // Longitude for New York City 
-  const latRange = 0.1; // Range for latitude 
-  const lngRange = 0.1; // Range for longitude
+  const newYork = { lat: 40.7128, lng: -74.0060 }; 
+  const centralLat = 40.7128;  
+  const centralLng = -74.0060; 
+  const latRange = 0.1; 
+  const lngRange = 0.1; 
 
   for (let i = 0; i < num; i++) {
     const dob = faker.date.birthdate({ min: 18, max: 65, mode: 'age' });
@@ -22,10 +22,6 @@ const useGenerateProfiles = (num) => {
       dob: dob.toISOString().split('T')[0], // Format date as YYYY-MM-DD 
       age: new Date().getFullYear() - dob.getFullYear(),
       address: `${faker.location.streetAddress()}, ${faker.location.city()}, ${faker.location.state()}`,
-      // location: { 
-      //   lat: faker.location.latitude(newYork.lat - 0.05, newYork.lat + 0.05), // Random latitude within 0.05 degrees of New York City 
-      //   lng: faker.location.longitude(newYork.lng - 0.05, newYork.lng + 0.05) // Random longitude within 0.05 degrees of New York City 
-      // }
       location: { 
         lat: (centralLat - (latRange / 2)) + (Math.random() * latRange), 
         lng: (centralLng - (lngRange / 2)) + (Math.random() * lngRange) 
--- src/hooks/useGetCoordinates.js
+++ src/hooks/useGetCoordinates.js
@@ -1,43 +1,5 @@
 import loadGoogleMaps from "../utils/loadGoogleMaps";
 
- const geocodeAddress  = async (address) => {
-    const apiKey = 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg'; // Replace with your actual Google API key
-    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`);
-    const data = await response.json();
-  
-    if (data.status === 'OK') {
-      const location = data.results[0].geometry.location;
-      return { lat: location.lat, lng: location.lng };
-    } else {
-      console.log(data);
-      
-      throw new Error('Failed to fetch coordinates');
-    }
-  };
-
-  const geocodeAddress2 = async (address) => {
-    await loadGoogleMaps(); // Ensure Google Maps API is loaded
-    const geocoder = new window.google.maps.Geocoder();
-    geocoder.geocode({ address: address }, (results, status) => {
-      if (status === "OK") {
-
-        const location = results[0].geometry.location;
-        // console.log(location.lat());
-        const latitude = location.lat();
-        const longitude = location.lng();
-        // const coordinates: { lat: location.lat(), lng: location.lng() }
-        
-        return location;
-        // setFormData((prevData) => ({
-        //   ...prevData,
-        //   location: { lat: location.lat(), lng: location.lng() }
-        // }));
-      } else {
-        console.error("Geocode was not successful for the following reason: " + status);
-      }
-    });
-  };
-
 
 export const useGetCoordinates = async (address) => {
     await loadGoogleMaps(); // Ensure Google Maps API is loaded
